ced333387d506b6abb1fa134eb562bb119c2616d
F0x <Operator-F0x@proton.me>
2024-09-26 15:15:07 +0200

    Delete Backend/Bar_Gestion directory

--- Backend/Bar_Gestion/Bar_Gestion/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Articolo(models.Model):
-    titolo = models.CharField(max_length=100)
-    contenuto = models.TextField()
-
-    def __str__(self):
-        return self.titolo
