f18c4a22e2e0902f40ca5d08380f51769a4a9434
F0x <Operator-F0x@proton.me>
2024-09-30 13:26:24 +0200

    Refactory code structure and directory tree

--- Backend/Restaurant_Admin/menu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import juice, menucold,menuhot, special
-
-admin.site.register(menucold)
-admin.site.register(menuhot)
-admin.site.register(juice)
-admin.site.register(special)
