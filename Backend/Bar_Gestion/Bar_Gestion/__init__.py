2704ad2ea4eb9e2d863c1a0813cf280169fe7549
F0x <Operator-F0x@proton.me>
2024-09-30 12:36:57 +0200

    Delete Backend/Bar_Gestion directory
    non utilizzato

--- Backend/Bar_Gestion/Bar_Gestion/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Package for Bar_Gestion.
-"""
--- Backend/Bar_Gestion/Bar_Gestion/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def frontend(request):
-    return render(request,'Bar_Gestional_Manager/FrontEnd/myapp/src/index.html')
